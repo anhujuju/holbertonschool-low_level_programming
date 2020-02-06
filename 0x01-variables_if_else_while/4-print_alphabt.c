5e7b83023ee67701680afe3a79e6d5a3fa49ef32
Andres <Anhujuju1@gmail.com>
2020-02-06 20:43:33 +0000

    delete e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - start of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	if (ch != 'e' && ch != 'q')
+	putchar (ch);
+}
+	putchar('\n');
+	return (0);
+}
