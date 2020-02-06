569187923c0dd2af16983518b351299f3e07c1c8
Andres <Anhujuju1@gmail.com>
2020-02-06 21:27:27 +0000

    put '0'

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - start of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int in;
+
+	for (in = 0 ; in <= 9 ; in++)
+{
+	putchar(in+'0');
+}
+	putchar('\n');
+	return (0);
+}
