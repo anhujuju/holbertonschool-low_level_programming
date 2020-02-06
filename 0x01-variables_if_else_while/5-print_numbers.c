35d8deeb6ee0ce2c4eb43663fc55aae38d292209
Andres <Anhujuju1@gmail.com>
2020-02-06 20:55:28 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - start of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int in;
+
+	for (in = 0 ; in <= 9 ; in++)
+{
+	printf("%d", in);
+}
+	putchar('\n');
+	return (0);
+}
