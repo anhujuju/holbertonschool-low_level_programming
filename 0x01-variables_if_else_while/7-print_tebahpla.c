0d6984b6b6e2e380b55b563ec9b8f0dee2afbd26
Andres <Anhujuju1@gmail.com>
2020-02-06 21:38:52 +0000

    reverce

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - start of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z' ; ch >= 'a' ; ch--)
+{
+	putchar(ch);
+}
+	putchar('\n');
+	return (0);
+}
