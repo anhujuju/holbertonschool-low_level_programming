0a7b88e3789ad62c5f3cd05bd5a296ed256b9265
Andres <Anhujuju1@gmail.com>
2020-02-06 20:35:41 +0000

    uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - start of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar (ch);
+}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+	putchar (ch);
+}
+	putchar('\n');
+	return (0);
+}
