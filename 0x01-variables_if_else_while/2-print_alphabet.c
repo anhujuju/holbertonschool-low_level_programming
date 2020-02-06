ef286f3e0084c7a555b5b8dcfc7b0365544e736d
Andres <Anhujuju1@gmail.com>
2020-02-06 20:26:42 +0000

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - start of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar (ch);
+	putchar('\n');
+}
+	return (0);
+}
