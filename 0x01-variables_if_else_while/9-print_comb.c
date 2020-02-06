84a8d23ddfa8e03039dfb2a0bbebcdb22c45421b
Andres <Anhujuju1@gmail.com>
2020-02-06 22:20:05 +0000

    spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - start of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int in;
+
+	for (in = 48 ; in < 58 ; in++)
+{
+	putchar(in);
+	if(in < 57)
+{
+	putchar(44);
+	putchar(32);
+}
+}
+	putchar('\n');
+	return (0);
+}
