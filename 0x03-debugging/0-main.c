a8a50e411f18c8294267f07134d36ac0ffd11fbc
Andres <Anhujuju1@gmail.com>
2020-02-11 21:26:30 +0000

    delete spaces

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -7,9 +6,9 @@
 
 int main(void)
 {
-	int i;
+int i;
 
-	i = 0;
-	positive_or_negative(i);
-	return (0);
+i = 0;
+positive_or_negative(i);
+return (0);
 }
