e1247242ffc6b960fc6652b9045563111260c284
Andres <Anhujuju1@gmail.com>
2020-02-13 20:25:02 +0000

    agree description

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,13 @@
-#include "holberton.h"
-
+include "holberton.h"
 /**
-* _isupper - checks for uppercase character
-* @c : character received
-*Return: 1 is uppercase 0 otherwhise
+* _isdigit - check if is a number
+* @c: character received
+*Return: 0 (success)
 */
-
 int _isdigit(int c)
 {
-	if (c <= 48 && c <= 57)
-
-	{
-	return (1);
-	}
+	if (c >= '0' && c <= '9')
+		return (1);
 	else
-	return (0);
+		return (0);
 }
