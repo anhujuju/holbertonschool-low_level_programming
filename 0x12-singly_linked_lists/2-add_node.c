61e4b1753bbaff018b2b3d66e12a2a6c24c18b73
Andres <anhujuju1gmai.com>
2020-03-19 20:05:26 +0000

    solve betty

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -26,21 +26,21 @@ unsigned int _strlen(char *str)
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
+list_t *new;
 
-	if (str == NULL)
-		return (NULL);
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	new->len = _strlen(new->str);
-	new->next = *head;
-	*head = new;
-	return (new);
+if (str == NULL)
+return (NULL);
+new = malloc(sizeof(list_t));
+if (new == NULL)
+return (NULL);
+new->str = strdup(str);
+if (new->str == NULL)
+{
+free(new);
+return (NULL);
+}
+new->len = _strlen(new->str);
+new->next = *head;
+*head = new;
+return (new);
 }
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -23,29 +23,29 @@ int _strlen(const char *s)
   */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *current;
-	char *dupstr;
+list_t *new, *current;
+char *dupstr;
 
-	if (str == NULL)
-		return (NULL);
-	dupstr = strdup(str);
-	if (dupstr == NULL)
-		return (NULL);
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-	new->str = dupstr;
-	new->len = _strlen(str);
-	new->next = NULL;
+if (str == NULL)
+return (NULL);
+dupstr = strdup(str);
+if (dupstr == NULL)
+return (NULL);
+new = malloc(sizeof(list_t));
+if (new == NULL)
+return (NULL);
+new->str = dupstr;
+new->len = _strlen(str);
+new->next = NULL;
 
-	if (*head == NULL)
-	{
-		*head = new;
-		return (*head);
-	}
-	current = *head;
-	while (current->next != NULL)
-		current = current->next;
-	current->next = new;
-	return (*head);
+if (*head == NULL)
+{
+*head = new;
+return (*head);
+}
+current = *head;
+while (current->next != NULL)
+current = current->next;
+current->next = new;
+return (*head);
 }
